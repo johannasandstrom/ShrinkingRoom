88f32c6546d05204c0d2fd2badf20ff803428737
olkor_ <olgakorobeinik@gmail.com>
2019-06-26 10:56:02 +0200

    Startpoint added

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -2,8 +2,8 @@ public class Timer {
     static Thread thread = new Thread();
 
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 60; i >= 0; i--)
-        {
+        System.out.println("GO GO GO");
+        for (int i = 60; i >= 0; i--) {
             thread.sleep(1000);
             System.out.println(i);
         }
