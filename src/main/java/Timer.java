553847401990f06bc7b3a0b5de4c989e89a0d1d6
olkor_ <olgakorobeinik@gmail.com>
2019-06-26 15:35:45 +0200

    code fix...nothing spec.

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -3,9 +3,9 @@ public class Timer {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("GO GO GO");
-        for (int i = 60; i >= 0; i--) {
+        for (int sek = 60; sek >= 0; sek--) {
             thread.sleep(1000);
-            System.out.println(i);
+            System.out.println(sek);
         }
     }
 }
